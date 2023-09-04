e18a105cfae519bab126392ba669f795f3b3f3d8
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2023-09-04 17:01:04 -0500

    updated open ai package and code

--- README.md
+++ README.md
@@ -6,12 +6,7 @@ Crafting minds for Minecraft with AI!
 
 Install Node.js >= 14 from [nodejs.org](https://nodejs.org/)
 
-Then, install mineflayer
-```
-npm install mineflayer
-npm install mineflayer-pathfinder
-npm install mineflayer-collectblock
-```
+Install node modules with `npm install`
 
 ## Usage
 
@@ -19,4 +14,4 @@ Start minecraft server on localhost port `55916`
 
 Add `OPENAI_API_KEY` (and optionally `OPENAI_ORG_ID`) to your environment variables.
 
-run `npm main.js`
+run `node main.js`
--- utils/gpt.js
+++ utils/gpt.js
@@ -1,16 +1,16 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAIApi from 'openai';
 
 
-var openAiConfig = null;
+let openAiConfig = null;
 if (process.env.OPENAI_ORG_ID) {
-    openAiConfig = new Configuration({
+    openAiConfig = {
         organization: process.env.OPENAI_ORG_ID,
         apiKey: process.env.OPENAI_API_KEY,
-    });
+    };
 } else {
-    openAiConfig = new Configuration({
+    openAiConfig = {
         apiKey: process.env.OPENAI_API_KEY,
-    });
+    };
 }
 const openai = new OpenAIApi(openAiConfig);
 
@@ -28,16 +28,16 @@ export async function sendRequest(turns, systemMessage, stop_seq) {
 
     let res = null;
     try {
-        let completion = await openai.createChatCompletion({
+        let completion = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             messages: messages,
             stop: stop_seq,
         });
-        res = completion.data.choices[0].message.content;
+        res = completion.choices[0].message.content;
     }
     catch (err) {
         console.log(err);
-        res = 'I amm sorry, I do not know how to respond to that.';
+        res = 'I am sorry, I do not know how to respond to that.';
     }
     return res;
 }
