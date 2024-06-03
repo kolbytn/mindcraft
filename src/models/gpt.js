3342a6deb9b76a0b990a2be754caeeb9f4fd31a1
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2024-06-03 18:40:01 -0500

    removed strict format

--- src/models/gpt.js
+++ src/models/gpt.js
@@ -1,6 +1,5 @@
 import OpenAIApi from 'openai';
 import { getKey, hasKey } from '../utils/keys.js';
-import { strictFormat } from '../utils/text.js';
 
 export class GPT {
     constructor(model_name, url) {
