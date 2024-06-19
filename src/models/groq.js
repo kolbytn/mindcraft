52bb0e009edfc4af6098088aae5d356f754fdf69
Copper <157772455+FateUnix29@users.noreply.github.com>
2024-06-18 17:56:04 -0700

    basic groq commit here

---
+++ src/models/groq.js
@@ -0,0 +1,29 @@
+const Groq = require('groq-sdk');
+
+const groq = new Groq();
+async function main() {
+  const chatCompletion = await groq.chat.completions.create({
+    "messages": [
+      {
+        "role": "system",
+        "content": "i like grapes"
+      },
+      {
+        "role": "user",
+        "content": ""
+      }
+    ],
+    "model": "mixtral-8x7b-32768",
+    "temperature": 0.85,
+    "max_tokens": 8192,
+    "top_p": 1,
+    "stream": true,
+    "stop": null
+  });
+
+  for await (const chunk of chatCompletion) {
+    process.stdout.write(chunk.choices[0]?.delta?.content || '');
+  }
+}
+
+main();
