50b75faa756f51c24fa9a7a56993d9d3fe722aa9
Vineeth <94882582+FantomWolf182@users.noreply.github.com>
2024-08-25 14:14:08 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,7 @@ export default
     "auth": "offline", // or "microsoft"
     
     "profiles": [
-        //"./andy.json",
-        "./profiles/dolphin.json" // Groq support, mixtral model
+        "./andy.json"
         
         // add more profiles here, check ./profiles/ for more
         // more than 1 profile will require you to /msg each bot indivually
