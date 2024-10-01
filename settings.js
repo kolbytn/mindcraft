9fa655080031887cadace7b8256061a016dc8a8a
AlakBlinder <Alak.Blinder@gmail.com>
2024-09-30 19:12:05 -0700

    Adding Profile Generator

--- settings.js
+++ settings.js
@@ -1,22 +1,14 @@
-export default 
-{
-    "minecraft_version": "1.20.4", // supports up to 1.20.4
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916,
-    "auth": "offline", // or "microsoft"
-    
-    "profiles": [
-        "./andy.json",
-        // add more profiles here, check ./profiles/ for more
-        // more than 1 profile will require you to /msg each bot indivually
-    ],
-    "load_memory": false, // load memory from previous session
-    "init_message": "Say hello world and your name", // sends to all on spawn
-
-    "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
-    "code_timeout_mins": 10, // minutes code is allowed to run. -1 for no timeout
-    
-    "max_commands": -1, // max number of commands to use in a response. -1 for no limit
-    "verbose_commands": true, // show full command syntax
-    "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
-}
+export default {
+  "minecraft_version": "1.20.4",
+  "host": "45.159.6.47",
+  "port": 25822,
+  "auth": "offline",
+  "profiles": [],
+  "load_memory": false,
+  "init_message": "Say hello world and your name",
+  "allow_insecure_coding": false,
+  "code_timeout_mins": 10,
+  "max_commands": -1,
+  "verbose_commands": true,
+  "narrate_behavior": true
+};
