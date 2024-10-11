fb8c1e053b6992796ad3874ad81797fc890ba2ab
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2024-10-10 22:28:47 -0500

    default show view false

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ export default
     "init_message": "Say hello world and your name", // sends to all on spawn
 
     "language": "en", // translate to/from this language. Supports these language names: https://cloud.google.com/translate/docs/languages
-    "show_bot_views": true, // show bot's view in browser at localhost:3000, 3001...
+    "show_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
 
     "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
     "code_timeout_mins": 10, // minutes code is allowed to run. -1 for no timeout
