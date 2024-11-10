cba7f7b431d61219083c6d970d331eb19322bdba
Qu Yi <quyimail@foxmail.com>
2024-11-10 17:58:23 +0800

    Default settings except code_timeout_mins

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ export default
     "show_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
 
     "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
-    "code_timeout_mins": 3, // minutes code is allowed to run. -1 for no timeout，set 1.Set 1 min to timely code adjustments
+    "code_timeout_mins": 3, // minutes code is allowed to run. -1 for no timeout，set 3.Set 3 min to timely code adjustments
 
     "max_messages": 15, // max number of messages to keep in context
     "max_commands": -1, // max number of commands to use in a response. -1 for no limit
