3cc12c6bd6752b7156d861fc1ee39572bce0f7eb
Isadora White <isadora.c.white@gmail.com>
2024-12-27 20:42:34 -0600

    fixing bugs with the initial blueprint validation functions

--- src/agent/commands/actions.js
+++ src/agent/commands/actions.js
@@ -422,6 +422,7 @@ export const actionsList = [
         description: 'Check if the level is complete and what blocks still need to be placed for the blueprint',
         perform: runAsAction(async (agent, levelNum) => {
             return await checkLevelBlueprint(agent, levelNum);
+            //todo: if not complete, explain what still needs to be done
         })
     }, 
     {
