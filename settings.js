abb170238d6c8957e698ac2d4b120c41b0ba169c
RodoHallow <rodonoxbox@gmail.com>
2025-02-19 02:00:53 +0000

    Pending changes exported from your codespace

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 export default 
 {
-    "minecraft_version": "1.20.4", // supports up to 1.21.1
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916,
+    "minecraft_version": "1.21.1", // supports up to 1.21.1
+    "host": "gonorreia.falixsrv.me", // or "localhost", "your.ip.address.here"
+    "port": 32052,
     "auth": "offline", // or "microsoft"
 
     // the mindserver manages all agents and hosts the UI
@@ -27,12 +27,12 @@ export default
         // using more than 1 profile requires you to /msg each bot indivually
         // individual profiles override values from the base profile
     ],
-    "load_memory": false, // load memory from previous session
+    "load_memory": true, // load memory from previous session
     "init_message": "Respond with hello world and your name", // sends to all on spawn
     "only_chat_with": [], // users that the bots listen to and send general messages to. if empty it will chat publicly
     
     "language": "en", // translate to/from this language. Supports these language names: https://cloud.google.com/translate/docs/languages
-    "show_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
+    "show_bot_views": true, // show bot's view in browser at localhost:3000, 3001...
 
     "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
     "code_timeout_mins": -1, // minutes code is allowed to run. -1 for no timeout
@@ -42,6 +42,6 @@ export default
     "num_examples": 2, // number of examples to give to the model
     "max_commands": -1, // max number of commands that can be used in consecutive responses. -1 for no limit
     "verbose_commands": true, // show full command syntax
-    "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
+    "narrate_behavior": false, // chat simple automatic actions ('Picking up item!')
     "chat_bot_messages": true, // publicly chat messages to other bots
 }
