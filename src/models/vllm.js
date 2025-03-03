05998b6c54aa8ecdc6bba7af6b1d6019bcb5d7e7
Isadora White <isadora.c.white@gmail.com>
2025-03-03 05:35:15 +0000

    vllm support added

---
+++ src/models/vllm.js
@@ -0,0 +1,54 @@
+// This code uses Dashscope and HTTP to ensure the latest support for the Qwen model.
+// Qwen is also compatible with the OpenAI API format;
+
+import OpenAIApi from 'openai';
+import { getKey, hasKey } from '../utils/keys.js';
+
+export class VLLM {
+    constructor(model_name, url) {
+        this.model_name = model_name;
+
+        // Currently use self-hosted SGLang API for text generation; use OpenAI text-embedding-3-small model for simple embedding.
+        let vllm_config = {};
+        if (url)
+            vllm_config.baseURL = url;
+        else
+            vllm_config.baseURL = 'http://0.0.0.0:8000/v1';
+
+        vllm_config.apiKey = ""
+
+        this.vllm = new OpenAIApi(vllm_config);
+    }
+
+    async sendRequest(turns, systemMessage, stop_seq = '***') {
+        let messages = [{ 'role': 'system', 'content': systemMessage }].concat(turns);
+
+        const pack = {
+            model: this.model_name || "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
+            messages,
+            stop: stop_seq,
+        };
+
+        let res = null;
+        try {
+            console.log('Awaiting openai api response...')
+            // console.log('Messages:', messages);
+            let completion = await this.vllm.chat.completions.create(pack);
+            if (completion.choices[0].finish_reason == 'length')
+                throw new Error('Context length exceeded');
+            console.log('Received.')
+            res = completion.choices[0].message.content;
+        }
+        catch (err) {
+            if ((err.message == 'Context length exceeded' || err.code == 'context_length_exceeded') && turns.length > 1) {
+                console.log('Context length exceeded, trying again with shorter context.');
+                return await this.sendRequest(turns.slice(1), systemMessage, stop_seq);
+            } else {
+                console.log(err);
+                res = 'My brain disconnected, try again.';
+            }
+        }
+        return res;
+    }
+
+}
