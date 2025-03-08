684cd414935fac202278026f7cc0f74d7449abdb
Ayush Maniar <ayushmaniar70@gmail.com>
2025-03-07 19:28:08 -0800

    Added blocked_actions empty list back to settings.js

--- settings.js
+++ settings.js
@@ -36,6 +36,7 @@ export default
     "show_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
 
     "allow_insecure_coding": process.env.INSECURE_CODING || false, // allows newAction command and model can write/run code on your computer. enable at own risk
+    "blocked_actions": [],
     "code_timeout_mins": -1, // minutes code is allowed to run. -1 for no timeout
     "relevant_docs_count": 5, // Parameter: -1 = all, 0 = no references, 5 = five references. If exceeding the maximum, all reference documents are returned.
 
