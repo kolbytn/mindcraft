ef37a400b0459388f3ec117fb97a3c2c103f68bb
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2025-03-13 15:42:23 -0500

    fix digdown

--- src/agent/commands/actions.js
+++ src/agent/commands/actions.js
@@ -409,8 +409,8 @@ export const actionsList = [
     },
     {
         name: '!digDown',
-        description: 'Digs down a specified distance.',
-        params: {'distance': { type: 'int', description: 'Distance to dig down'}},
+        description: 'Digs down a specified distance. Will stop if it reaches lava, water, or a fall of >=4 blocks below the bot.',
+        params: {'distance': { type: 'int', description: 'Distance to dig down', domain: [1, Number.MAX_SAFE_INTEGER] }},
         perform: runAsAction(async (agent, distance) => {
             await skills.digDown(agent.bot, distance)
         })
