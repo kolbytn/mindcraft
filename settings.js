b2de1cda17e6d9e0064c607dea92e938370211b7
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2025-06-13 13:08:44 -0500

    clean settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const settings = {
     // the mindserver manages all agents and hosts the UI
     "mindserver_port": 8080,
     
-    // the base profile is shared by all bots for default prompts/examples/modes
+    "base_profile": "survival", // survival, creative, or god_mode
     "profiles": [
         "./andy.json",
         // "./profiles/gpt.json",
@@ -23,28 +23,26 @@ const settings = {
         // individual profiles override values from the base profile
     ],
 
-    // agent settings
-    "base_profile": "survival", // survival, creative, or god_mode
     "load_memory": false, // load memory from previous session
     "init_message": "Respond with hello world and your name", // sends to all on spawn
     "only_chat_with": [], // users that the bots listen to and send general messages to. if empty it will chat publicly
     "speak": false, // allows all bots to speak through system text-to-speech. works on windows, mac, on linux you need to `apt install espeak`
     "language": "en", // translate to/from this language. Supports these language names: https://cloud.google.com/translate/docs/languages
+    "render_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
+
+    "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
     "allow_vision": false, // allows vision model to interpret screenshots as inputs
     "blocked_actions" : ["!checkBlueprint", "!checkBlueprintLevel", "!getBlueprint", "!getBlueprintLevel"] , // commands to disable and remove from docs. Ex: ["!setMode"]
+    "code_timeout_mins": -1, // minutes code is allowed to run. -1 for no timeout
     "relevant_docs_count": 5, // number of relevant code function docs to select for prompting. -1 for all
+
     "max_messages": 15, // max number of messages to keep in context
     "num_examples": 2, // number of examples to give to the model
     "max_commands": -1, // max number of commands that can be used in consecutive responses. -1 for no limit
-    "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
-    "log_all_prompts": false, // log ALL prompts to file
     "verbose_commands": true, // show full command syntax
-    "chat_bot_messages": true, // publicly chat bot-to-bot messages
-
-    // mindserver settings
-    "render_bot_view": false, // show bot's view in browser at localhost:3000, 3001...
-    "allow_insecure_coding": true, // allows newAction command and model can write/run code on your computer. enable at own risk
-    "code_timeout_mins": -1, // minutes code is allowed to run. -1 for no timeout
+    "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
+    "chat_bot_messages": true, // publicly chat messages to other bots
+    "log_all_prompts": false, // log ALL prompts to file
 }
 
 export default settings;
