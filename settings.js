f092dad4501f0a8639678d8156034ce3d3c8041c
Ayman33433 <131425467+Ayman33433@users.noreply.github.com>
2025-12-28 09:24:48 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
     "minecraft_version": "auto", // or specific version like "1.21.6"
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916, // set to -1 to automatically scan for open ports
+    "host": "Ayman01011.aternos.me", // or "localhost", "your.ip.address.here"
+    "port": 14032, // set to -1 to automatically scan for open ports
     "auth": "offline", // or "microsoft"
 
     // the mindserver manages all agents and hosts the UI
