81603b887ef8f45d30addccc637ddc527facabf1
gamershifin75-stack <gamershifin75@gmail.com>
2026-07-23 03:34:27 +0530

    Update server connection to Shifineyy.aternos.me:46856

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
     "minecraft_version": "auto", // or specific version like "1.21.6"
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916, // set to -1 to automatically scan for open ports
+    "host": "Shifineyy.aternos.me", // or "localhost", "your.ip.address.here"
+    "port": 46856, // set to -1 to automatically scan for open ports
     "auth": "offline", // or "microsoft"
 
     // the mindserver manages all agents and hosts the UI
