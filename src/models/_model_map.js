112c7979ae069cd322f742ecba5c45224340a28f
Cloudy <52116030+xdCloudy@users.noreply.github.com>
2026-08-17 21:31:30 +0100

    Pass API key alias through model factory

--- src/models/_model_map.js
+++ src/models/_model_map.js
@@ -84,6 +84,6 @@ export function createModel(profile) {
     if (!apiMap[profile.api]) {
         throw new Error('Unknown api:', profile.api);
     }
-    const model = new apiMap[profile.api](profile.model, profile.url, profile.params);
+    const model = new apiMap[profile.api](profile.model, profile.url, profile.params, profile.api_key_alias);
     return model;
 }
